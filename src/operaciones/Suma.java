1981e8291b7d5cae8347a4aa7aabbb7906de788a
Irene-Toca <61660913+Irene-Toca@users.noreply.github.com>
2021-01-22 21:02:41 +0100

    javadoc de la clase suma actualizado

--- src/operaciones/Suma.java
+++ src/operaciones/Suma.java
@@ -1,23 +1,40 @@
 package operaciones;
 /**
- * Clase 1: Suma. Esta clase implementar todos los metodos que estan relacionados con la sumas de la calculadora.
+ * Esta clase implementara todos los metodos que estan relacionados con la sumas de la calculadora
+ * @author irene  este es el nombre del autor 
+ * @version 1.0
+ * @since 17- 01- 2021 
  * 
- * @author irene
- *
+ * Creamos la  clase  Suma 
+ * 
+ * 
+ * declaramos las variavles
+ * 
+ * double resultado  es lo que nos dara al sumar  los dos numeros reales en el primer metodo
+ * tambien lo podremos reutilizar para sumar los tres numeros reales en el tercer metodo 
+ * 
+ * int solucion       es lo que nos dara al sumar  los dos numeros entero 
+ * 
+ * static int acumulador = 0 este empieza en cero e iremos acumulando segun los numeros que pasemos por consola 
+ * 
  */
 public class Suma {	
 	static int acumulador = 0;
-	
 	double resultado;
 	int solucion;
 	
 	
+	
+	
 	/**
-	 * Sumamos dos numeros reales
 	 * 
-	 * @param num1 primer numero real a sumar
-	 * @param num2 segundo numero real a sumar
-	 * @return  double, suma de (num1, num2)
+	 * Creamos un metodo  double  Suma2Reales
+	 * al cual le pasamos por consola  dos numeros reales para poderlos sumar 
+	 * 
+	 * @param num1  es un double y es el primer numero real a sumar
+	 * @param num2  es un double y es el segundo numero real a sumar
+	 * @return      Nos devuelve  double como resultado  ya que es la  suma de dos numeros  reales  (num1, num2) que retornan
+	 * 
 	 */
 	public double suma2Reales(double num1, double num2) {
 		resultado = num1 + num2;
@@ -26,11 +43,14 @@ public class Suma {
 	
 	
 	/**
-	 * Sumamos 2 numeros Enteros
 	 * 
-	 * @param num1 primer numero real a sumar
-	 * @param num2 segundo numero real a sumar
-	 * @return int, suma de (num1, num2) 
+	 * Creamos un metodo int Sumamos2Enteros
+	 * al cual le pasamos por consola  dos numeros enteros  para poderlos suma
+	 * 
+	 * @param num1  es un int primer numero entero a sumar
+	 * @param num2  es un int segundo numero entero a sumar
+	 * @return      Nos devuelve  int como solucion ya que es la  suma de dos numeros enteros  (num1, num2) que retornan
+	 * 
 	 */
 	public int suma2Enteros(int num1, int num2) {
 		solucion = num1 + num2;
@@ -40,24 +60,33 @@ public class Suma {
 	
 	
 	/**
-	 *  Sumamos tres numeros reales
+	 * Creamos un metodo  double  Suma3Reales
+	 * al cual le pasamos por consola  tres numeros reales para poderlos sumar 
 	 * 
-	 * @param num1 primer numero real a sumar
-	 * @param num2 segundo numero real a sumar
-	 * @param num3 terser numero  real a sumar
-	 * @return  double, suma de (num1, num2, num3)
+	 * @param num1  es un double y es el primer numero real a sumar
+	 * @param num2  es un double y es el segundo numero real a sumar
+	 * @param num3  es un double y es el tercer numero real a sumar
+	 * @return      Nos devuelve  double como resultado  ya que es la  suma de tres  numeros 
+	 *              reales  (num1, num2, num3) que retornan
+	 * 
 	 */
 	public double suma3Reales(double num1, double num2, double num3) {
 		resultado = num1 + num2 + num3;
 		return resultado;
+		
+		
+		
 	}
 	
 	
 	/**
-	 * Obtenemos el valor acumulado
 	 * 
-	 * @param num valor auadido al acumulador 
-	 * @return int el total del acumulador
+	 * Creamos un metodo int valorAcumulado que (int num)
+	 * 
+	 * @param  num valor que nos pasan por consola y los vamos sumando almenos 3 veses  ejemplo  3+4=7+2=9
+	
+	 * @return Nos devuelve int el total del acumulador que retorna y es 9
+	 * 
 	 */
 
 	public int valorAcumulado(int num) {

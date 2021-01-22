95235b3a7d88ce488335095183d6813e851ba95b
Roberto Gonzalez <robertello@hotmail.com>
2021-01-22 17:25:35 +0100

    Añadido demas comentarios de JavaDoc en los metodos

--- src/operaciones/Resta.java
+++ src/operaciones/Resta.java
@@ -2,48 +2,79 @@ package operaciones;
 
 /**
  * Esta clase definirá las operaciones relacionadas con la resta
- * 
+ *
  * @author rober
  ** @version 1.0
  * @since 18-01-2021 .
  */
 public class Resta {
-	
-	/**
-	 * Usando la variable entera acumuLador iremos restando numeros 
-	 */
-    static int acumuLador = 0;
-    
+
     /**
-	 * Variable de tipo double para restar los dos numeros reales
-	 */
-	double resultado;
-	
-	 /**
-	  * Variable de tipo integer para restar los numeros enteros
-	  */
-	int solucion;
-	
-	
-	public double resta2Reales(double num1, double num2) {
-		resultado = num1 - num2;
-		return resultado;
-	}
-        
-        public int resta2Enteros(int num1, int num2){
-                solucion=num1 - num2;
-                return solucion;
-        }
-        
-        public double resta3Reales(double num1, double num2, double num3) {
-		resultado = num1 - num2 - num3;
-		return resultado;
-	}
-        
-        public int restaAcumulado(int num){
-            Resta.acumuLador= Resta.acumuLador - (num);
-            return Resta.acumuLador;
-            
-        }
-        
+     * Usando la variable entera acumuLador iremos restando numeros
+     */
+    static int acumuLador = 0;
+
+    /**
+     * Variable de tipo double para restar los dos numeros reales
+     */
+    double resultado;
+
+    /**
+     * Variable de tipo integer para restar los numeros enteros
+     */
+    int solucion;
+
+    /**
+     * En este metodo restaré dos variables de tipo double y devolvere un
+     * resultado
+     *
+     * @param num1 - Valor introducido por el usuario
+     * @param num2 - Valor introducido por el usuario
+     * @param resultado - Resultado de restar num1 menos num2
+     */
+    public double resta2Reales(double num1, double num2) {
+
+        return resultado;
+    }
+
+    /**
+     * En este metodo restaré dos variables de tipo entero y devolvere un
+     * resultado
+     *
+     * @param num1 - Valor introducido por el usuario
+     * @param num2 - Valor introducido por el usuario
+     * @param solucion - Resultado de restar num1 menos num2
+     */
+    public int resta2Enteros(int num1, int num2) {
+
+        return solucion;
+    }
+
+    /**
+     * En este metodo restaré tres variables de tipo double y devolvere un
+     * resultado
+     *
+     * @param num1 - Valor introducido por el usuario
+     * @param num2 - Valor introducido por el usuario
+     * @param num3 - Valor introducido por el usuario
+     * @param solucion - Resultado de restar num1 menos num2 menos num3
+     */
+    public double resta3Reales(double num1, double num2, double num3) {
+
+        return resultado;
+    }
+
+    /**
+     * En este metodo ire restando variables de tipo entero hasta obtener un
+     * resultado
+     *
+     * @param num - Valor introducido por el usuario
+     * @param acumulador - Iremos restando los numeros hasta llegar a 0
+     */
+    public int restaAcumulado(int num) {
+
+        return Resta.acumuLador;
+
+    }
+
 }

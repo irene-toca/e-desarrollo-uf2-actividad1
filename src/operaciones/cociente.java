39804db73ada52cb47c7d7dfe6ffd7f090831e63
PedroMurilloArnau <76122975+PedroMurilloArnau@users.noreply.github.com>
2021-01-22 20:59:19 +0100

    Add files via upload

---
+++ src/operaciones/cociente.java
@@ -0,0 +1,59 @@
+package operaciones;
+
+/**
+*" En ésta clase se define las operaciones relacionada con la division entre dos números
+*el inverso de un número
+*la raíz de un número
+*
+* @author Pedro
+** @version 1.0
+* since 18-01-2021
+*/
+public class cociente {
+	/** Usaremos la variable entera acumulador para dividir los terminos
+	 * 
+	 */
+	static int acumulador = 0;
+	
+		/** Utilizamos la variable double para dividir dos números reales
+		 * 	de los cuales
+		 * @param num1 = al valor introducido correspondiente al dividendo
+		 * @param num2 = al valor introducido correspondiente al divisor
+		 * Importante no introducir el valor 0
+		 * @param resultado = Resultado de dividir los dos terminos
+		 */
+	public double division2Reales (double num1, double num2){
+		
+		return resultado;
+	}
+	/** Utilizamos la variable de tipo entero para dividir dos números enteros
+		 * 	de los cuales
+		 * @param num1 = al valor introducido correspondiente al dividendo
+		 * @param num2 = al valor introducido correspondiente al divisor
+		 * Importante no introducir el valor 0
+		 * @param resultado = Resultado de dividir los dos terminos
+		 */
+	public int division2Enteros (int num1, int num2){
+		
+		return resultado;
+	}
+	/** Utilizamos la variable doble para calcular el termino del cual querems
+	 *conocer su inverso
+	 *@param num1 = el valar introducido del que queremos conocer el inverso
+	 *@param resultado = el inverso del número introducido
+	 */
+	public double inverso1Real (double num1){
+	
+		return resultado;
+	}
+	/** Utilizaremos una variable entera para calcular la raiz cuadrada de un
+	 * número del cual queremos conocer su raiz cuadrada
+	 * @param num1 = valor introducido del que queremos conocer la raiz cuadrada
+	 * @param num2 = Resultado del raiz cuadrada del número 
+	 */
+	public int raiz1Entero (int num1)
+	
+	return resultado;
+	
+	
+	

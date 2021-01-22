455c21833af3e75b2366c49746df1d2815a43dbf
Irene-Toca <61660913+Irene-Toca@users.noreply.github.com>
2021-01-22 21:38:50 +0100

    actualizacion del javadoc

---
+++ src/operaciones/Cociente.java
@@ -0,0 +1,65 @@
+package operaciones;
+
+/**
+* En esta clase se define las operaciones relacionada con la division entre dos numeros
+* el inverso de un numero
+* la raiz de un numero
+*
+* @author Pedro
+* @version 1.0
+* since 18-01-2021
+*/
+public class Cociente {
+	private double resultado = 0;
+	private int res = 0;
+
+	/** 
+	 * Usaremos la variable entera acumulador para dividir los terminos
+	 * 
+	 */
+	static int acumulador = 0;
+	
+		/** Utilizamos la variable double para dividir dos numeros reales
+		 * 	de los cuales
+		 * @param num1 = al valor introducido correspondiente al dividendo
+		 * @param num2 = al valor introducido correspondiente al divisor
+		 * Importante no introducir el valor 0
+		 * @return resultado = Resultado de dividir los dos terminos
+		 */
+	public double division2Reales (double num1, double num2){
+		
+		return resultado;
+	}
+	/** 
+	 * Utilizamos la variable de tipo entero para dividir dos numeros enteros
+	 * de los cuales
+	 * @param num1 = al valor introducido correspondiente al dividendo
+	 * @param num2 = al valor introducido correspondiente al divisor
+	 * Importante no introducir el valor 0
+	 * @return res = Resultado de dividir los dos terminos
+	 */
+	public int division2Enteros (int num1, int num2){
+		
+		return res;
+	}
+	/** Utilizamos la variable doble para calcular el termino del cual querems
+	 * conocer su inverso
+	 * @param num1 = el valor introducido del que queremos conocer el inverso
+	 * @return resultado = el inverso del número introducido
+	 */
+	public double inverso1Real (double num1){
+	
+		return resultado;
+	}
+	/** Utilizaremos una variable entera para calcular la raiz cuadrada de un
+	 * numero del cual queremos conocer su raiz cuadrada
+	 * @param num1 = valor introducido del que queremos conocer la raiz cuadrada
+	 * @return res = Resultado del raiz cuadrada del número 
+	 */
+	public int raiz1Entero (int num1) {
+	
+		return res;
+	}
+	
+}
+	
